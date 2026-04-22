bbbe5d021eb5f638c43d682fc401cb8c00f40115
Ellen Suh <ellensuh@bu.edu>
2026-04-21 23:42:42 -0400

    Fixing styled-components bug I was getting

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    reactCompiler: true,
 
-  images: {
-    remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.unsplash.com',
-        },
-    ],
-  },
+    compiler: {
+        styledComponents: true,
+    },
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
